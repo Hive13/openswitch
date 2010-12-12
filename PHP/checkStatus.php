a7c32dba696f24471e6e4f99619095ea4ddd806e
Paul Vincent <pvince@gmail.com>
2010-12-11 23:03:45 -0500

    Updated the open image.

--- PHP/checkStatus.php
+++ PHP/checkStatus.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysql_connect('localhost', 'username','password');
+$con = mysql_connect('127.0.0.1', 'root','PennyArcade23');
 if(!$con) {
    die("Bork Bork! " . mysql_error());
 }
@@ -11,7 +11,7 @@ $sql="select * from doorEvents order by dtEventDate DESC limit 1";
 $res = mysql_query($sql);
 $row = mysql_fetch_array($res);
 if($row['bDoorIsOpen'] == 1) {
-	echo '<img src=open.jpg width=100px height=100px>';
+	echo '<img src=open.png width=100px height=100px>';
 } else {
 	echo '<img src=closed.jpg width=100px height=100px>';
 }
