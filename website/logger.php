c84ef606b9f56a9a8e973258eb59f055e46cc167
ryodoan <pvince@gmail.com>
2012-05-23 13:30:34 -0400

    Added the ability to log a passed in temperature

--- website/logger.php
+++ website/logger.php
@@ -29,4 +29,27 @@
       
     }  
   }
+
+  if(isSet($_REQUEST['temp'])) {
+    if($_REQUEST['temp'] == '1') {
+      // if = 1, read the last temperature
+      echo "Temperature Report:\n";
+      $qry = "SELECT dcTemp, vcLocation FROM tempEvents
+              ORDER BY dtEventDate DESC LIMIT 1"; // Temp hack until I figure out multiple locations
+      $res = mysql_query($qry, $dbh);
+      while( $row = mysql_fetch_array($res, MYSQL_ASSOC)) {
+        echo "Location: ".$row['vcLocation']."  Temperature: ".$row['dcTemp']."\n";
+      }
+      mysql_close($dbh);
+    } else {
+      // Add whatever was passed in to the text file
+      echo "Saving temperature...\n";
+      $safeTemp = mysql_real_escape_string($_REQUEST['temp']);
+      $qry = "INSERT INTO tempEvents (dcTemp, vcLocation)
+              VALUES ($safeTemp, 'DoorSwitch')";
+      $res = mysql_query($qry,$dbh);
+      mysql_close($dbh);
+      echo "$safeTemp";
+    }
+  }
 ?>
